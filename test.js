f41d85bae4147f1a0772e24ad3bf04035f9d485c
Hunor Karamán <hunix94@gmail.com>
2018-01-16 17:26:00 +0200

    comments

--- test.js
+++ test.js
@@ -6,6 +6,13 @@ var str = `ID  NAME         EMAIL
 001 RANDOM JERRY jerry@mail.com
 `
 
+var str_c = `ID  NAME         EMAIL
+---
+000 RANDOM JOE   joe@mail.com
+# some comment
+001 RANDOM JERRY jerry@mail.com
+`
+
 var obj = [ { ID: '000', NAME: 'RANDOM JOE', EMAIL: 'joe@mail.com' },
   			{ ID: '001', NAME: 'RANDOM JERRY', EMAIL: 'jerry@mail.com' } ]
 
@@ -13,6 +20,11 @@ test('parses the example', () => {
 	expect(datta.parse(str)).toEqual(obj)
 })
 
+test('parses comments', () => {
+	expect(datta.parse(str_c)).toEqual(obj)
+})
+
 test('stringifies the example', () => {
 	expect(datta.stringify(obj)).toEqual(str)
 })
+
